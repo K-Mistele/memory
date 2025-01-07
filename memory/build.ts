bf7cc8f472e43fe2880a51d8f9b492df46c32265
Kyle Mistele <kyle@mistele.com>
2025-01-07 14:23:09 -0600

    feat: skeleton package

---
+++ memory/build.ts
@@ -0,0 +1,20 @@
+import type {BuildConfig} from 'bun'
+
+const defaultBuildConfig: BuildConfig = {
+    entrypoints: ['./src/index.ts'],
+    outdir: './dist',
+    target: 'node',
+}
+
+await Promise.all([
+    Bun.build({
+        ...defaultBuildConfig,
+        format: 'esm',
+        naming: '[dir]/[name].js'
+    }),
+    Bun.build({
+        ...defaultBuildConfig,
+        format: 'cjs',
+        naming: '[dir]/[name].cjs'
+    })
+])
