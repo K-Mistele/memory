27f1d106b9c3e8c6297a29f9a61e5884a7cded8c
Kyle Mistele <kyle@mistele.com>
2025-01-16 12:02:51 -0600

    feat: mem0 self-hosted config

--- mem0/main.py
+++ mem0/main.py
@@ -4,20 +4,24 @@ from dotenv import load_dotenv
 
 load_dotenv('../.env')
 
+NEO4J_PASSWORD = os.environ.get("NEO4J_PASSWORD")
+POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+
 m = Memory.from_config(config_dict={
     "graph_store": {
         "provider": "neo4j",
         "config": {
             "url": "neo4j://localhost:7687",
             "username": "neo4j",
-            "password": os.environ.get("NEO4J_PASSWORD")
+            "password": NEO4J_PASSWORD
         },
     },
     "vector_store": {
         "provider": "pgvector",
         "config": {
             "user": "postgres",
-            "password": "password",
+            "password": POSTGRES_PASSWORD,
             "dbname": "postgres",
             "host": "localhost",
             "port": 5432,
@@ -28,13 +32,19 @@ m = Memory.from_config(config_dict={
     "llm": {
         "provider": "openai",
         "config": {
-
+            "model": "text-embedding-3-small",
+            "embedding_dims": 2048,
+            "api_key": OPENAI_API_KEY,
+            "openai_base_url": "https://api.openai.com/v1",  # also for litellm
         }
     },
     "embedder": {
-        "provider": "openai",
+        "provider": "openai", # or, litellm -- same parameters
         "config": {
-
+            "model": "gpt-4o-mini",
+            "temperature": "0.7",
+            "api_key": OPENAI_API_KEY,
+            "openai_base_url": "https://api.openai.com/v1", # also for litellm
         }
     },
     "custom_prompt": "Please only extract entities related to technologies and user preferences.",
